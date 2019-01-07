63acad0419d5633d511a5a66c757d98da2d59430
reluem <lucas.rech@me.com>
2019-01-07 16:14:44 +0100

    add prices CTE
    Signed-off-by: reluem <lucas.rech@me.com>

--- src/Resources/contao/languages/de/tl_content.php
+++ src/Resources/contao/languages/de/tl_content.php
@@ -1,13 +1,15 @@
 <?php
     
+    /** Timetable */
+    
     $GLOBALS['TL_LANG']['CTE']['timetable'] = [
         'Zeitplan',
         'Zeitplan mit verschiedenen Zeitpunkten, die für verschiedene Tage definiert werden können',
     ];
-    $GLOBALS['TL_LANG']['tl_content']['timetable_legend'] = 'Zeitplan';
+    $GLOBALS['TL_LANG']['tl_content']['timetable_legend'] = 'Zeitplan anlegen';
     
     $GLOBALS['TL_LANG']['tl_content']['timetable'] = [
-        'Zeitplan anlegen',
+        'Zeitplan',
     ];
     $GLOBALS['TL_LANG']['tl_content']['timetable_date'] = [
         'Datum',
@@ -27,3 +29,29 @@
     $GLOBALS['TL_LANG']['tl_content']['timetable_loc'] = [
         'Ort',
     ];
+    
+    
+    /** Prices */
+    
+    
+    $GLOBALS['TL_LANG']['tl_content']['prices'] = [
+        'Preistabelle',
+    ];
+    $GLOBALS['TL_LANG']['tl_content']['prices_legend'] = 'Preise eingeben';
+    $GLOBALS['TL_LANG']['tl_content']['price'] = [
+        'Preis',
+    ];
+    $GLOBALS['TL_LANG']['tl_content']['price_desc'] = [
+        'Beschreibung',
+    ];
+    $GLOBALS['TL_LANG']['tl_content']['price_type'] = [
+        'Preisstufe',
+    ];
+    $GLOBALS['TL_LANG']['tl_content']['price_valid_from'] = [
+        'gültig von',
+    ];
+    $GLOBALS['TL_LANG']['tl_content']['price_valid_until'] = [
+        'gültig bis',
+    ];
+    
+    
